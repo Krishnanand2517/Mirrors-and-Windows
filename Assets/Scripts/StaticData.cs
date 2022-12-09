fc35b78ff1955254bec8ea9d81bca6600d4aa750
Xranitell <vozchic05@gmail.com>
2022-12-09 18:04:34 +0300

    Add portal(Mirror scripts) with sound and fade

---
+++ Assets/Scripts/StaticData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StaticData
+{
+    public static MirrorManager manager;
+    public static CanvasController canvasController;
+    public static GameObject player;
+    public static Mirror connectedMirror;
+}
